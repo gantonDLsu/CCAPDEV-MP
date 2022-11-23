6469f597d1e7781c77b6c82dfae5672257f285b8
Anton de Leon <iamgpmc@gmail.com>
2022-11-23 23:01:56 +0800

    asdf

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ const db = mysql.createConnection({
 
 const app = express();
 
-
-
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
