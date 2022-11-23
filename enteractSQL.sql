791b8e7e00614ff281aa1946c4ba795775fbaee9
Anton de Leon <iamgpmc@gmail.com>
2022-11-24 01:08:57 +0800

    asdf

---
+++ enteractSQL.sql
@@ -0,0 +1,140 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: enteract
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posts` (
+  `postid` int NOT NULL,
+  `user` int NOT NULL,
+  `message` varchar(255) COLLATE utf8mb3_bin NOT NULL,
+  `datetime` datetime NOT NULL,
+  `likes` int DEFAULT NULL,
+  PRIMARY KEY (`postid`,`user`),
+  KEY `userid_idx` (`user`),
+  CONSTRAINT `userid` FOREIGN KEY (`user`) REFERENCES `users` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usercomments`
+--
+
+DROP TABLE IF EXISTS `usercomments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usercomments` (
+  `userid` int NOT NULL,
+  `postid` int DEFAULT NULL,
+  `comment` varchar(255) COLLATE utf8mb3_bin DEFAULT NULL,
+  PRIMARY KEY (`userid`),
+  KEY `postid_idx` (`postid`),
+  CONSTRAINT `usercommentstouserspostid` FOREIGN KEY (`postid`) REFERENCES `posts` (`postid`),
+  CONSTRAINT `usercommentstousersuserid` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usercomments`
+--
+
+LOCK TABLES `usercomments` WRITE;
+/*!40000 ALTER TABLE `usercomments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usercomments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `userfollows`
+--
+
+DROP TABLE IF EXISTS `userfollows`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `userfollows` (
+  `followid` int NOT NULL,
+  `userid` int NOT NULL,
+  `following` int NOT NULL,
+  PRIMARY KEY (`followid`),
+  UNIQUE KEY `followid_UNIQUE` (`followid`),
+  KEY `userid_idx` (`userid`) /*!80000 INVISIBLE */,
+  KEY `userfollowstousersfollowing_idx` (`following`),
+  CONSTRAINT `userfollowstousersfollowing` FOREIGN KEY (`following`) REFERENCES `users` (`userid`),
+  CONSTRAINT `userfollowstousersuserid` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `userfollows`
+--
+
+LOCK TABLES `userfollows` WRITE;
+/*!40000 ALTER TABLE `userfollows` DISABLE KEYS */;
+/*!40000 ALTER TABLE `userfollows` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `userid` int NOT NULL,
+  `username` varchar(45) COLLATE utf8mb3_bin NOT NULL,
+  `password` varchar(45) COLLATE utf8mb3_bin NOT NULL,
+  `email` varchar(45) COLLATE utf8mb3_bin NOT NULL,
+  `icon` blob,
+  `name` varchar(45) COLLATE utf8mb3_bin DEFAULT NULL,
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-24  1:04:35
