d1bc4ea3fed3c7219551da70f4415ef0646fe5e5
Reece De Padua <reece_depadua@dlsu.edu.ph>
2022-11-23 19:00:03 +0800

    init

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const { render } = require('ejs');
+ 
+/*
+const db = mysql.createConnection({
+    connection: {
+        host: 'localhost',
+        user: 'root',
+        password: 'test',
+        database: 'enteract',
+    }
+})
+*/
+
+const app = express();
+
+
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+let intialPath = path.join(__dirname, "public");
+
+app.use(bodyParser.json());
+app.use(express.static(intialPath));
+
+let fullname;
+let username;
+
+app.get("/enteract.html", function (req, res){
+    res.render("index");
+});
+app.get("/signup.html", function (req, res){
+    res.render("signup");
+});
+
+app.post("/signup.html", function (req, res){
+    let data = {
+        name: req.body.name, 
+        email: req.body.email, 
+        user: req.body.username,
+        password: req.body.password
+    };
+    fullname = data.name;
+    username = data.user;
+    res.render("blogpage", { Name: data.name, userName: data.user});
+    res.redirect("/blogpage.html");
+});
+
+app.get("/aboutus.html", function (req, res){
+    res.render("aboutus");
+});
+app.get("/blogpage.html", function (req, res){
+    res.render("blogpage", { Name: fullname, userName: username});
+});
+
+
+app.listen(3000, () => console.log('listening on port 3000!'));
