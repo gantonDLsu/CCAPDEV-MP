b61edf98e073a0bbd35b1850e4f745e94bf63caa
Reece De Padua <reece_depadua@dlsu.edu.ph>
2022-12-07 18:47:01 +0800

    init

---
+++ enteractSQL.sql
@@ -0,0 +1,114 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: enteract
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posts` (
+  `postid` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `datetime` datetime NOT NULL,
+  PRIMARY KEY (`postid`),
+  KEY `userid_idx` (`username`),
+  CONSTRAINT `poststousersusername` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (1,'johndoe','John Doe','Hello world','2022-11-26 08:12:53'),(2,'marydoe','Mary Doe','Enteract is here!','2022-11-26 08:13:47'),(3,'gabrieldeleon','Gabriel Anton de Leon','This is a post','2022-11-26 08:14:56'),(4,'johndoe','John Doe','Definitely not a \"Lorem ipsum\" post','2022-11-26 08:17:45'),(5,'johndoe','John Doe','!@#$%^&*()-=','2022-11-26 08:19:07');
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usercomments`
+--
+
+DROP TABLE IF EXISTS `usercomments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usercomments` (
+  `commentid` int NOT NULL AUTO_INCREMENT,
+  `postid` int NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `comment` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`commentid`),
+  UNIQUE KEY `commentid_UNIQUE` (`commentid`),
+  KEY `postid_idx` (`postid`),
+  CONSTRAINT `usercommentstouserspostid` FOREIGN KEY (`postid`) REFERENCES `posts` (`postid`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usercomments`
+--
+
+LOCK TABLES `usercomments` WRITE;
+/*!40000 ALTER TABLE `usercomments` DISABLE KEYS */;
+INSERT INTO `usercomments` VALUES (1,1,'Mary Doe','Hello!'),(2,3,'John Doe','This is a comment'),(3,5,'John Doe',',./;\'[]<>?:\"{}'),(5,3,'Gabriel Anton de Leon','This is another comment'),(6,2,'Mary Doe','Enter-act yay!');
+/*!40000 ALTER TABLE `usercomments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `userid` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (2,'gabrieldeleon','12345','Gabriel Anton de Leon','gabriel_anton_deleon@dlsu.edu.ph'),(3,'juliadalipe','12345','Julia Bettina Dalipe','julia_dalipe@dlsu.edu.ph'),(4,'reecedepadua','12345','Reece Cendrick De Padua','reece_depadua@dlsu.edu.ph'),(5,'johndoe','12345','John Doe','johndoe@gmail.com'),(6,'marydoe','12345','Mary Doe','marydoe@gmail.com');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-26  8:23:02
---
+++ server.js
@@ -0,0 +1,204 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const { render } = require('ejs');
+ 
+
+const app = express();
+
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Hope2714612!', //CHANGE ACCORDING TO YOUR WORKBENCH PASSWORD
+    database : 'enteract', // input database name
+  });
+  
+  db.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+    else
+      console.log('connected as id' + db.threadId);
+  });
+  
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+let intialPath = path.join(__dirname, "public");
+
+app.use(bodyParser.json());
+app.use(express.static(intialPath));
+
+let name;
+let username;
+let userid;
+let posts;
+let comments = [];
+let arr = [];
+
+app.get("/", function (req, res){
+    res.render("index");
+});
+
+app.get("/enteract.ejs", function (req, res){
+    res.render("index");
+});
+app.get("/signup.ejs", function (req, res){
+    res.render("signup", {errorMessage: null});
+});
+
+app.post("/adduser", function (req, res){
+    let data = {
+        name: req.body.name, 
+        email: req.body.email, 
+        username: req.body.username,
+        password: req.body.password,
+    };
+
+    let sql = "INSERT INTO users SET ?";
+    let query = db.query(sql, data, (err, results) => {
+        try {
+            if (err) throw err;
+        }
+        catch (err) {
+            res.render("signup", {errorMessage: err});
+            return;
+        };
+        res.render("login", {errorMessage: "You must login first after signing in."});
+    });
+});
+
+app.get("/login.ejs", function (req, res){
+    res.render("login", {errorMessage: null});
+});
+
+app.post("/loginuser", function (req, res) {
+    let sql = "SELECT * FROM users WHERE username='"+req.body.username+"' AND password='"+req.body.password+"'";
+    let query = db.query(sql, (err, result) => {
+        if (err) throw err;
+
+        if (JSON.stringify(result) == "[]") {
+            res.render("login", {errorMessage: "User not found"});
+        }
+        else {
+            name = result[0].name;
+            username = result[0].username;
+            userid = result[0].userid;
+            res.redirect("blogpage.ejs");
+        };
+    });
+});
+
+app.get("/blogpage.ejs", function (req, res) {
+    let query = db.query ("SELECT username FROM users", (err, result) => {
+        if (err) throw err;
+        result.forEach(element => { arr.push(element.username); });
+        arr.sort((a, b) => 0.5 - Math.random()).splice(3);
+
+        let postquery = db.query("SELECT * FROM posts ORDER BY datetime DESC", (err, postresults) => {
+            posts = postresults;
+            let commentquery = db.query("SELECT * FROM usercomments", (err, commentresults) => {
+                comments = commentresults;
+                res.render("blogpage", {Name : name, userName : username, posts: posts, comments: comments, toFollow: arr, isEditingPost: false});
+            });
+        });
+    });
+});
+
+app.get("/aboutus.ejs", function (req, res){
+    res.render("aboutus");
+});
+
+app.post("/posting", function (req, res){
+    const today = new Date();
+    let data = {
+        username: username,
+        name: name,
+        message: req.body.message,
+        datetime: today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+' '+today.getHours()+':'+today.getMinutes()+':'+today.getSeconds(),
+    };
+    let sql = "INSERT INTO posts SET ?";
+    let query = db.query(sql, data, (err, results) => {
+        if (err) throw err;
+        let postquery = db.query("SELECT * FROM posts ORDER BY postid DESC", (err, results) => {
+            posts = results;
+            // res.render("blogpage", {Name : name, userName : username, posts: posts, comments: comments});
+            res.redirect("blogpage.ejs");
+        });
+    });
+});
+
+app.post("/addcomment:postid", function (req, res){
+    let data = {
+        postid: parseInt(req.params.postid.slice(1)),
+        name: name,
+        comment: req.body.comment
+    };
+    let query = db.query("INSERT INTO usercomments SET ?", data, (err, results) => {
+        if (err) throw err;
+        let commentquery = db.query("SELECT * FROM usercomments", (err, commentresults) => {
+            comments = commentresults;
+            res.redirect("blogpage.ejs");
+        });
+    })
+});
+
+app.post("/deletepost:postid", function (req, res){
+    let data = {
+        postid: parseInt(req.params.postid.slice(1))
+    };
+    let deletecomments = db.query("DELETE FROM usercomments WHERE ?", data, (err, result) => {
+        if (err) throw err;
+        let commentquery = db.query("SELECT * FROM usercomments", (err, results) => {
+            comments = results;
+        });
+    });
+    let query = db.query("DELETE FROM posts WHERE ?", data, (err, result) => {
+        if (err) throw err;
+        let postquery = db.query("SELECT * FROM posts ORDER BY postid DESC", (err, results) => {
+            posts = results;
+            res.redirect("blogpage.ejs");
+        });
+    });
+});
+
+app.post("/deletecomment:commentid", function (req, res){
+    let data = {
+        commentid: parseInt(req.params.commentid.slice(1))
+    };
+    let deletecomments = db.query("DELETE FROM usercomments WHERE ?", data, (err, result) => {
+        if (err) throw err;
+        let commentquery = db.query("SELECT * FROM usercomments", (err, results) => {
+            comments = results;
+            res.redirect("blogpage.ejs");
+        });
+    });
+});
+
+app.post("/editpost:postid", function (req, res){
+    let data = {
+        postid: parseInt(req.params.postid.slice(1))
+    };
+    let editpost = db.query("SELECT * FROM posts WHERE ?", data, (err, result) => {
+        if (err) throw err;
+
+        res.render("blogpage", {Name : name, userName : username, posts: posts, comments: comments, toFollow: arr, isEditingPost: true, editPostID: result[0].postid, editPostMessage: result[0].message});
+    })
+});
+
+app.post("/updatepost:postid", function (req, res){
+    let data = {
+        postid: parseInt(req.params.postid.slice(1))
+    };
+    let sql = "UPDATE posts SET message = \"" + req.body.message + "\" WHERE ?";
+    let editpost = db.query(sql, data, (err, result) => {
+        if (err) throw err;
+    });
+    res.redirect("blogpage.ejs");
+});
+
+app.listen(3000, () => console.log('listening on port 3000!')); 
